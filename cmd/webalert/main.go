70a0b3800a5f155aa962972f471d3628bc982510
Fabian König <fkoenig83@googlemail.com>
2026-09-08 10:26:11 +0200

    feat: pass changes and errors back to main

--- cmd/webalert/main.go
+++ cmd/webalert/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/fkdiy/webalert/internal/config"
@@ -10,6 +11,8 @@ import (
 )
 
 func main() {
+	log.SetFlags(0)
+
 	// Locate and parse config.yaml
 	conf, err := config.Load()
 
@@ -20,14 +23,35 @@ func main() {
 	mon := monitor.New(conf.Targets)
 
 	// Set initial state
-	mon.Check()
+	_, err = mon.Check()
 
+	if err != nil {
+		log.Printf("Some targets could not be initialized:\n\n%v\n\n", err)
+	}
+
+	// Initialize ticker with interval set in config
 	ticker := time.NewTicker(time.Duration(conf.Interval) * time.Second)
 	defer ticker.Stop()
 
 	fmt.Printf("Checking targets every %v seconds ...\n\n", conf.Interval)
 
+	// Run infinite check loop
 	for range ticker.C {
-		mon.Check()
+		changes, err := mon.Check()
+
+		if err != nil {
+			log.Printf("Some targets could not be checked:\n\n%v\n\n", err)
+		}
+
+		for _, change := range changes {
+			fmt.Printf(
+				"%v: Change registered in selector '%v'\nPrevious: %v\nCurrent: %v\nAlerting: %v\n\n",
+				change.Target.URL,
+				change.Target.Selector,
+				change.Previous,
+				change.Current,
+				strings.Join(conf.EMail.Recipients, ", "),
+			)
+		}
 	}
 }
--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -1,6 +1,9 @@
 package monitor
 
 import (
+	"errors"
+	"fmt"
+	"net"
 	"strings"
 
 	"github.com/fkdiy/webalert/internal/config"
@@ -19,30 +22,81 @@ func New(targets []config.TargetConfig) *Monitor {
 	}
 }
 
-func (m *Monitor) Check() {
-	for _, target := range m.targets {
-		m.checkTarget(target)
-	}
+type Change struct {
+	Target   config.TargetConfig
+	Previous string
+	Current  string
 }
 
-func (m *Monitor) checkTarget(target config.TargetConfig) {
+func (m *Monitor) Check() ([]Change, error) {
+	var changes []Change
+	var errs []error
+
+	for _, target := range m.targets {
+		change, changed, err := m.checkTarget(target)
+
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		if changed {
+			changes = append(changes, change)
+		}
+	}
+
+	return changes, errors.Join(errs...)
+}
+
+func (m *Monitor) checkTarget(target config.TargetConfig) (Change, bool, error) {
 	c := colly.NewCollector()
 
+	var current string
+	var found bool
+	var dnsErr *net.DNSError
+
 	c.OnHTML(target.Selector, func(e *colly.HTMLElement) {
-		current := strings.TrimSpace(e.Text)
-
-		previous, exists := m.states[target.URL]
-
-		if !exists {
-			m.states[target.URL] = current
-			return
-		}
-
-		if previous != current {
-			// later: send mail
-			m.states[target.URL] = current
-		}
+		current = strings.TrimSpace(e.Text)
+		found = true
 	})
 
-	c.Visit(target.URL)
+	err := c.Visit(target.URL)
+	if err != nil {
+		if errors.As(err, &dnsErr) {
+			return Change{}, false, fmt.Errorf(
+				"%s: %s",
+				target.URL,
+				dnsErr.Err,
+			)
+		} else {
+			return Change{}, false, err
+		}
+	}
+
+	if !found {
+		return Change{}, false, fmt.Errorf(
+			"%v: selector '%v' not found",
+			target.URL,
+			target.Selector,
+		)
+	}
+
+	previous, exists := m.states[target.URL]
+
+	if !exists {
+		m.states[target.URL] = current
+		return Change{}, false, nil
+	}
+
+	if previous != current {
+		m.states[target.URL] = current
+
+		return Change{
+			Target:   target,
+			Previous: previous,
+			Current:  current,
+		}, true, nil
+	}
+
+	return Change{}, false, nil
 }
